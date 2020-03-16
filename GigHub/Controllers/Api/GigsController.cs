b987f15f473c70229ffc120e81ef87dfe797e1af
ebrammamdouh <ebrammamdoh5@gmail.com>
2020-03-16 18:06:17 +0200

    add notification module

--- GigHub/Controllers/Api/GigsController.cs
+++ GigHub/Controllers/Api/GigsController.cs
@@ -1,4 +1,5 @@
 ﻿using GigHub.Models;
+using GigHub.Models.Notification;
 using Microsoft.AspNet.Identity;
 using System;
 using System.Collections.Generic;
@@ -25,6 +26,27 @@ namespace GigHub.Controllers.Api
             if (gig.IsCanceled)
                 return NotFound();
             gig.IsCanceled = true;
+            var notication = new Notification
+            {
+                DateTime = DateTime.Now,
+                Gig = gig,
+                NotificationType = NotificationType.GigCanceled,
+                OriginalDateTime = gig.DateTime,
+                OriginalVenue = gig.Venue
+            };
+            _context.Notifications.Add(notication);
+            var attendee = _context.Attendances.Where(t => t.GigId == gig.Id).Select(t => t.Attendee).ToList();
+            attendee.ForEach(user =>
+               {
+                   var userNotification = new UserNotification
+                   {
+                       Notification = notication,
+                       User = user
+                   };
+                   _context.UserNotifications.Add(userNotification);
+               });
+
+
             _context.SaveChanges();
 
             return Ok();
---
+++ GigHub/Models/Notification/UserNotification.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace GigHub.Models.Notification
+{
+    public class UserNotification
+    {
+        [Key]
+        [Column(Order = 1)]
+        public string UserId { get; set; }
+        public ApplicationUser User { get; set; }
+        [Key]
+        [Column(Order = 2)]
+        public int NotificationId { get; set; }
+        public Notification Notification { get; set; }
+        public bool IsReaded { get; set; }
+    }
+}
